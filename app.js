7ab9cc7c0ff14edf2b3ccd1ef17adfd96fd98588
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2021-11-05 11:48:43 +0100

    Updates

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import schema from './schema/schema.js'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 const app = express()
 const port = 3001
+
+app.use(cors())
 
 mongoose.connect('mongodb+srv://kamil:habib@kamil.x0n2s.mongodb.net/graphqldb?retryWrites=true&w=majority')
 mongoose.connection.once('open', () => {
